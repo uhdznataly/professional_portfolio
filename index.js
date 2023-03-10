1359c896b41754baa10a33cfa7f5ef95156fa39f
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-03-09 21:28:35 -0600

    add 3rd project

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function myFunction() {
     document.getElementById("picture").src = "./images/fakeimg.png";
 }
+
+function myFunction() {
+    document.getElementById("picture1").src = "./images/./images/Screenshot\ 2023-03-09\ at\ 5.56.43\ PM.png";
+}
